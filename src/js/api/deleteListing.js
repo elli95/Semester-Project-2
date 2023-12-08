8d301491fc52d16440b7625aed8c0e8fc0270bed
Elise Marie Hogsnes <elise-marie.h@hotmail.com>
2023-12-08 13:06:50 +0100

    delete listing

---
+++ src/js/api/deleteListing.js
@@ -0,0 +1,27 @@
+import { API_LISTINGS_URL } from "./constant-api.mjs";
+import { apiData } from "../api/apiCall.mjs";
+
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+const listingId = params.get("id");
+
+const listingDataUrl = `${API_LISTINGS_URL}/${listingId}`;
+
+// const errorMessage = document.querySelector("#error-message");
+const deleteListingBtn = document.querySelector("#delete-listing-btn");
+deleteListingBtn.addEventListener("click", deleteListing);
+
+async function deleteListing() {
+  //   event.preventDefault();
+  try {
+    const method = "DELETE";
+    const listingDeletion = await apiData(listingDataUrl, method);
+    console.log(listingDeletion);
+
+    if (listingDeletion === undefined) {
+      window.location.replace("../../../profile/index.html");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
