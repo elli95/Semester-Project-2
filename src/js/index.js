9712772d2cb3720d0b2e5e96033f90e32e0d15f5
Elise Marie Hogsnes <elise-marie.h@hotmail.com>
2023-12-08 21:10:11 +0100

    removed some comments

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 import { API_LISTINGS_URL } from "./api/constant-api.mjs";
 import { apiData } from "./api/apiCall.mjs";
 import { listingsCard } from "./htmlStyle.js";
+// import { getCountdownDate } from "./dateCountdown.js";
 
+// console.log("getProfileData", getCountdownDate(endDateCountdown));
 const method = "GET";
 const listingsDataUrl = `${API_LISTINGS_URL}` + "?_bids=true&sort=created";
 
@@ -10,9 +12,14 @@ async function getProfileData(listingsDataUrl, method, data) {
   try {
     const listingsData = await apiData(listingsDataUrl, method, data);
     Object.values(listingsData).forEach(function (listing) {
+      // const listingdate = new Date(listing.endsAt).getTime();
       // console.log("123", listing);
 
+      // setInterval(function () {
+      // const listingsDate = getCountdownDate(listingdate);
+      //   console.log("123", listingsDate);
       listingsCard(listing);
+      // }, 1000);
     });
   } catch (error) {
     console.log(error);
