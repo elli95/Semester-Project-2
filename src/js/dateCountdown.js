186cc6404414740229d21357a17a3774e3e7b1b8
Elise Marie Hogsnes <elise-marie.h@hotmail.com>
2023-12-08 21:07:32 +0100

    listing date countdown js

---
+++ src/js/dateCountdown.js
@@ -0,0 +1,37 @@
+// Refrence: How to create a countdown timer using JavaScript "https://www.educative.io/answers/how-to-create-a-countdown-timer-using-javascript"
+
+function getCountdownDate(listing, infoContainer, endData) {
+  try {
+    setInterval(function () {
+      const listingdate = new Date(listing.endsAt).getTime();
+      const currentDate = new Date().getTime();
+      const timeDifferent = listingdate - currentDate;
+
+      const days = Math.floor(timeDifferent / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((timeDifferent % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((timeDifferent % (1000 * 60 * 60)) / (1000 * 60));
+      //   const seconds = Math.floor((timeDifferent % (1000 * 60)) / 1000);
+
+      const endDateCountdown = days + ":" + hours + ":" + minutes;
+      infoContainer.querySelector(endData).innerText = `Ends in: ${endDateCountdown}`;
+    }, 1000);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export { getCountdownDate };
+
+//
+//   const currentDateData = new Date();
+//   const currentDate = currentDateData.toLocaleString("en-GB", endDateSetup);
+
+//   if (currentDate < listingEndDate) {
+//     const endData = "h4";
+//     getCountdownDate(listing, infoContainer, endData);
+//   } else {
+//     infoContainer.querySelector("h4").innerText = `Ended`;
+//   }
+
+// const endData = "h3";
+// getCountdownDate(listingData, infoContainer, endData);
