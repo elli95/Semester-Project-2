f4ec83aa01be6407e645adad76877c8ebf24c84d
Elise Marie Hogsnes <elise-marie.h@hotmail.com>
2023-12-09 21:20:57 +0100

    commented out old search field

--- src/js/search.js
+++ src/js/search.js
@@ -5,30 +5,30 @@ import { listingsCard, headerSearch } from "./htmlStyle.js";
 const method = "GET";
 const listingsDataUrl = `${API_LISTINGS_URL}` + "?_bids=true&sort=created";
 
-const searchFieldHeader = document.querySelector("#search-input-header");
+// const searchFieldHeader = document.querySelector("#search-input-header");
 const searchField = document.querySelector("#search-input");
-searchFieldHeader.addEventListener("input", listingSearchHeader);
+// searchFieldHeader.addEventListener("input", listingSearchHeader);
 
 if (location.pathname === `/` || location.pathname === `/index.html`) {
   searchField.addEventListener("input", listingSearch);
 }
 
-async function listingSearchHeader() {
-  try {
-    const auctionListings = await apiData(listingsDataUrl, method);
-    const value = searchFieldHeader.value.toLowerCase();
-    console.log(value);
+// async function listingSearchHeader() {
+//   try {
+//     const auctionListings = await apiData(listingsDataUrl, method);
+//     const value = searchFieldHeader.value.toLowerCase();
+//     console.log(value);
 
-    let result = auctionListings.filter((inputText) => inputText.title.toLowerCase().includes(value));
+//     let result = auctionListings.filter((inputText) => inputText.title.toLowerCase().includes(value));
 
-    // document.getElementById("listingsSection").innerText = "";
-    Object.values(result).forEach(function (listing) {
-      headerSearch(listing);
-    });
-  } catch (error) {
-    console.log(error);
-  }
-}
+//     // document.getElementById("listingsSection").innerText = "";
+//     Object.values(result).forEach(function (listing) {
+//       headerSearch(listing);
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
 async function listingSearch() {
   try {
