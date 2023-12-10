b8b6d96cfa8255e2cfb2880296dee8430e42d5c1
Elise Marie Hogsnes <elise-marie.h@hotmail.com>
2023-12-11 00:37:33 +0100

    More precise countdown for specific listing

--- src/js/dateCountdown.js
+++ src/js/dateCountdown.js
@@ -12,7 +12,7 @@ function getCountdownDate(listing, infoContainer, endData) {
       const minutes = Math.floor((timeDifferent % (1000 * 60 * 60)) / (1000 * 60));
       //   const seconds = Math.floor((timeDifferent % (1000 * 60)) / 1000);
 
-      const endDateCountdown = days + ":" + hours + ":" + minutes;
+      const endDateCountdown = days + "D " + hours + "H " + minutes + "M";
       infoContainer.querySelector(endData).innerText = `Ends in: ${endDateCountdown}`;
     }, 1000);
   } catch (error) {
@@ -20,7 +20,27 @@ function getCountdownDate(listing, infoContainer, endData) {
   }
 }
 
-export { getCountdownDate };
+function getPreciseCountdownDate(listing, infoContainer, endData) {
+  try {
+    setInterval(function () {
+      const listingdate = new Date(listing.endsAt).getTime();
+      const currentDate = new Date().getTime();
+      const timeDifferent = listingdate - currentDate;
+
+      const days = Math.floor(timeDifferent / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((timeDifferent % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((timeDifferent % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((timeDifferent % (1000 * 60)) / 1000);
+
+      const endDateCountdown = days + " Days " + hours + " Hours " + minutes + " Min " + seconds + " Sec";
+      infoContainer.querySelector(endData).innerText = `Ends in: ${endDateCountdown}`;
+    }, 1000);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export { getCountdownDate, getPreciseCountdownDate };
 
 //
 //   const currentDateData = new Date();
