3ae0f61ebf449f4414020c9863b0f87dada3ec6a
Elise Marie Hogsnes <elise-marie.h@hotmail.com>
2023-12-11 00:38:30 +0100

    Better filter button design/reaction

--- src/js/profileFilter.js
+++ src/js/profileFilter.js
@@ -1,57 +1,91 @@
 import { API_PROFILE_URL } from "./api/constant-api.mjs";
 import { apiData } from "./api/apiCall.mjs";
 import { getLocalStorage } from "./localStorage.mjs";
+import { profileListingsCard, profileBidCard, profileWinCard } from "./htmlStyle.js";
 
 const method = "GET";
 const user = getLocalStorage("profile").name;
 const userDataUrl = `${API_PROFILE_URL}/${user}`;
 
-const showAllFilter = document.querySelector("#show-all-filter");
+// const showAllFilter = document.querySelector("#show-all-filter");
 const myBidsFilter = document.querySelector("#my-bids-filter");
 const myListingsFilter = document.querySelector("#my-listings-filter");
 const myWinsFilter = document.querySelector("#my-wins-filter");
 
-showAllFilter.addEventListener("click", showAll);
+// showAllFilter.addEventListener("click", showAll);
 myBidsFilter.addEventListener("click", myBids);
 myListingsFilter.addEventListener("click", myListings);
 myWinsFilter.addEventListener("click", myWins);
 
-async function showAll() {
+// async function showAll() {
+//   try {
+//     const userData = await apiData(userDataUrl, method);
+
+//     document.getElementById("my-listing-container").style.display = "flex";
+//     document.getElementById("my-bid-container").style.display = "flex";
+//     document.getElementById("my-bid-section-title").style.textAlign = "start";
+
+//     if (userData.wins.length > 0) {
+//       document.getElementById("my-wins-container").style.display = "flex";
+//       document.getElementById("my-wins-section-title").style.textAlign = "start";
+//     } else {
+//       document.getElementById("my-wins-container").style.display = "none";
+//       document.getElementById("my-wins-filter").style.display = "none";
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+const endDateSetup = {
+  second: "numeric",
+  minute: "numeric",
+  hour: "numeric",
+  day: "numeric",
+  month: "numeric",
+  year: "numeric",
+};
+
+async function myBids() {
   try {
-    const userData = await apiData(userDataUrl, method);
+    const userBids = await apiData(userDataUrl + "/bids?_listings=true", method);
+    const lastBid = userBids.filter((obj, index) => userBids.findIndex((bid) => bid.listing.id === obj.listing.id) === index);
 
-    document.getElementById("my-listing-container").style.display = "flex";
-    document.getElementById("my-bid-container").style.display = "flex";
-    document.getElementById("my-bid-section-title").style.textAlign = "start";
+    document.getElementById("my-listings-filter").style.backgroundColor = "#bfedce";
+    document.getElementById("my-bids-filter").style.backgroundColor = "#72f88d";
+    document.getElementById("my-wins-filter").style.backgroundColor = "#bfedce";
+    document.getElementById("listingsSection").innerText = "";
 
-    if (userData.wins.length > 0) {
-      document.getElementById("my-wins-container").style.display = "flex";
-      document.getElementById("my-wins-section-title").style.textAlign = "start";
-    } else {
-      document.getElementById("my-wins-container").style.display = "none";
-      document.getElementById("my-wins-filter").style.display = "none";
-    }
+    Object.values(lastBid).forEach(function (bidData) {
+      const currentDateData = new Date();
+      let listingdate = new Date(bidData.listing.endsAt);
+      const currentDate = currentDateData.toLocaleString("en-GB", endDateSetup);
+      const listingEndDate = listingdate.toLocaleString("en-GB", endDateSetup);
+
+      if (currentDate < listingEndDate) {
+        profileBidCard(bidData);
+      }
+    });
   } catch (error) {
     console.log(error);
   }
 }
 
-function myBids() {
+async function myListings() {
   try {
-    document.getElementById("my-listing-container").style.display = "none";
-    document.getElementById("my-bid-container").style.display = "flex";
-    document.getElementById("my-wins-container").style.display = "none";
-    document.getElementById("my-bid-section-title").style.textAlign = "end";
-  } catch (error) {
-    console.log(error);
-  }
-}
+    const listingData = await apiData(userDataUrl + "/listings?_bids=true", method);
 
-function myListings() {
-  try {
-    document.getElementById("my-listing-container").style.display = "flex";
-    document.getElementById("my-bid-container").style.display = "none";
-    document.getElementById("my-wins-container").style.display = "none";
+    document.getElementById("my-listings-filter").style.backgroundColor = "#72f88d";
+    document.getElementById("my-bids-filter").style.backgroundColor = "#bfedce";
+    document.getElementById("my-wins-filter").style.backgroundColor = "#bfedce";
+    document.getElementById("listingsSection").innerText = "";
+
+    Object.values(listingData).forEach(function (listing) {
+      profileListingsCard(listing);
+    });
+    //   document.getElementById("my-listing-container").style.display = "flex";
+    //   document.getElementById("my-bid-container").style.display = "none";
+    //   document.getElementById("my-wins-container").style.display = "none";
   } catch (error) {
     console.log(error);
   }
@@ -59,10 +93,23 @@ function myListings() {
 
 async function myWins() {
   try {
-    document.getElementById("my-listing-container").style.display = "none";
-    document.getElementById("my-bid-container").style.display = "none";
-    document.getElementById("my-wins-container").style.display = "flex";
-    document.getElementById("my-wins-section-title").style.textAlign = "end";
+    const userData = await apiData(userDataUrl, method);
+    const userBids = await apiData(userDataUrl + "/bids?_listings=true", method);
+
+    document.getElementById("my-listings-filter").style.backgroundColor = "#bfedce";
+    document.getElementById("my-bids-filter").style.backgroundColor = "#bfedce";
+    document.getElementById("my-wins-filter").style.backgroundColor = "#72f88d";
+    document.getElementById("listingsSection").innerText = "";
+
+    const myWins = userData.wins;
+    const auctionWins = userBids.filter((bids) => myWins.includes(bids.listing.id));
+    Object.values(auctionWins).forEach(function (winData) {
+      profileWinCard(winData);
+    });
+    // document.getElementById("my-listing-container").style.display = "none";
+    // document.getElementById("my-bid-container").style.display = "none";
+    // document.getElementById("my-wins-container").style.display = "flex";
+    // document.getElementById("my-wins-section-title").style.textAlign = "end";
   } catch (error) {
     console.log(error);
   }
