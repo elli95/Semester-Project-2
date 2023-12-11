5c6195ff1d29fe4f238ca94911fa3a36607f80cb
Elise Marie Hogsnes <elise-marie.h@hotmail.com>
2023-12-11 21:46:43 +0100

    Added more filter option

--- src/js/filter.js
+++ src/js/filter.js
@@ -5,22 +5,28 @@ import { listingsCard } from "./htmlStyle.js";
 const method = "GET";
 const listingsDataUrl = `${API_LISTINGS_URL}` + "?_bids=true&sort=created";
 
-const newPost = document.querySelector("#new-post");
-const oldPost = document.querySelector("#old-post");
-const popularPost = document.querySelector("#popular-post");
+const newListing = document.querySelector("#new-listing");
+const endingSoon = document.querySelector("#ending-soon-filter");
+const popularListing = document.querySelector("#popular-listing");
+const lowPrice = document.querySelector("#low-price");
+const highPrice = document.querySelector("#high-price");
 
-newPost.addEventListener("click", newFilter);
-oldPost.addEventListener("click", endingFilter);
-popularPost.addEventListener("click", popularFilter);
+newListing.addEventListener("click", newFilter);
+endingSoon.addEventListener("click", endingFilter);
+popularListing.addEventListener("click", popularFilter);
+lowPrice.addEventListener("click", lowFilter);
+highPrice.addEventListener("click", highFilter);
 
 async function newFilter() {
   try {
     const listingData = await apiData(listingsDataUrl, method);
     const result = listingData.sort((a, b) => new Date(b.created) - new Date(a.created));
 
-    document.getElementById("new-post").style.backgroundColor = "#72f88d";
-    document.getElementById("old-post").style.backgroundColor = "#bfedce";
-    document.getElementById("popular-post").style.backgroundColor = "#bfedce";
+    document.getElementById("new-listing").style.backgroundColor = "#72f88d";
+    document.getElementById("ending-soon-filter").style.backgroundColor = "#bfedce";
+    document.getElementById("popular-listing").style.backgroundColor = "#bfedce";
+    document.getElementById("low-price").style.backgroundColor = "#bfedce";
+    document.getElementById("high-price").style.backgroundColor = "#bfedce";
 
     printFilterResult(result);
   } catch (error) {
@@ -42,9 +48,11 @@ export async function endingFilter() {
     const listingData = await apiData(listingsDataUrl, method);
     const result = listingData.sort((a, b) => new Date(a.endsAt) - new Date(b.endsAt));
 
-    document.getElementById("new-post").style.backgroundColor = "#bfedce";
-    document.getElementById("old-post").style.backgroundColor = "#72f88d";
-    document.getElementById("popular-post").style.backgroundColor = "#bfedce";
+    document.getElementById("new-listing").style.backgroundColor = "#bfedce";
+    document.getElementById("ending-soon-filter").style.backgroundColor = "#72f88d";
+    document.getElementById("popular-listing").style.backgroundColor = "#bfedce";
+    document.getElementById("low-price").style.backgroundColor = "#bfedce";
+    document.getElementById("high-price").style.backgroundColor = "#bfedce";
 
     document.getElementById("listingsSection").innerText = "";
 
@@ -68,10 +76,110 @@ async function popularFilter() {
     const listingData = await apiData(listingsDataUrl, method);
     const result = listingData.sort((a, b) => b._count.bids - a._count.bids);
 
-    document.getElementById("new-post").style.backgroundColor = "#bfedce";
-    document.getElementById("old-post").style.backgroundColor = "#bfedce";
-    document.getElementById("popular-post").style.backgroundColor = "#72f88d";
+    console.log(result);
+    document.getElementById("new-listing").style.backgroundColor = "#bfedce";
+    document.getElementById("ending-soon-filter").style.backgroundColor = "#bfedce";
+    document.getElementById("popular-listing").style.backgroundColor = "#72f88d";
+    document.getElementById("low-price").style.backgroundColor = "#bfedce";
+    document.getElementById("high-price").style.backgroundColor = "#bfedce";
     printFilterResult(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function lowFilter() {
+  try {
+    const listingData = await apiData(listingsDataUrl, method);
+    const result = listingData.sort((a, b) => b._count.bids - a._count.bids);
+
+    document.getElementById("new-listing").style.backgroundColor = "#bfedce";
+    document.getElementById("ending-soon-filter").style.backgroundColor = "#bfedce";
+    document.getElementById("popular-listing").style.backgroundColor = "#bfedce";
+    document.getElementById("low-price").style.backgroundColor = "#72f88d";
+    document.getElementById("high-price").style.backgroundColor = "#bfedce";
+    printFilterResult(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function highFilter() {
+  try {
+    const listingData = await apiData(listingsDataUrl, method);
+
+    console.log(listingData);
+    // console.log(test);
+    // const result = listingData.sort((a, b) => a.bids[listingData._count.bids - 1].amount - b.bids.amount[listingData._count.bids - 1]);
+    // console.log(result);
+    // const test2 = listingData.bids.length > 0 ? listingData.bids.reduce((max, bid) => Math.max(max, bid.amount), 0) : 0;
+    // console.log("test", test2);
+
+    // let bidListCollection = [];
+    // let highestListingBid = [];
+    // Object.values(listingData).forEach(function (bidsData) {
+    //   // console.log(bidsData._count.bids);
+    //   // const lastBidNr = bidsData._count.bids;
+
+    //   console.log(bidsData);
+
+    //   const test2 = Math.max(bidsData.bids.amount);
+    //   console.log("test", test2);
+
+    // // console.log(lastBidNr);
+    // // console.log("222", lastBidNr - 1);
+    // // if (lastBidNr.length) {
+    // // console.log("tst", bidsData.bids[lastBidNr - 1]);
+    // for (let i = 0; i < bidsData.bids.length; i++) {
+    //   // console.log("tst", bidsData.bids[i]);
+    //   const listingBids = bidsData.bids[i];
+    //   // console.log("...............", listingBids);
+    //   // console.log("asdfsadf", Math.max(highestBid[0].amount));
+    //   bidListCollection.push(listingBids);
+    //   console.log("...............", bidListCollection);
+    // const highestBid = bidListCollection.sort((a, b) => b.amount - a.amount);
+    // console.log("tst----", highestBid[0].amount);
+    // highestListingBid.push(...highestBid);
+    // console.log("...............", highestListingBid);
+    // return highestBid[0];
+    // }
+    // }
+
+    // console.log("--------------", highestBid[0]);
+
+    //   if (bidsData._count.bids > 1) {
+    //     console.log("1", bidsData);
+    //     let bidListCollection = [];
+    //     bidListCollection.push(listingData.bids);
+    //     // const highestBid = bidListCollection[0].sort((a, b) => b.amount - a.amount);
+    //     console.log("2", bidListCollection);
+    //     // Object.values(bidsData).forEach(function (data) {
+    //     //   console.log(data);
+    //     // const result = bidsData.sort((a, b) => a.bids.amount - b.bids.amount);
+    //     // console.log(result);
+    // });
+    //   }
+
+    //   const result = bidsData.sort((a, b) => a.bids.amount - b.bids.amount);
+    //   console.log(result);
+    //   console.log(bidsData);
+    // });
+
+    // const result = listingData.sort((a, b) =>
+    //   Object.values(listingData.bids).forEach(function (bidsData) {
+    //     a.bidsData.amount - b.bidsData.amount;
+    //   })
+    // );
+
+    // console.log(listingData);
+    // console.log(result);
+
+    document.getElementById("new-listing").style.backgroundColor = "#bfedce";
+    document.getElementById("ending-soon-filter").style.backgroundColor = "#bfedce";
+    document.getElementById("popular-listing").style.backgroundColor = "#bfedce";
+    document.getElementById("low-price").style.backgroundColor = "#bfedce";
+    document.getElementById("high-price").style.backgroundColor = "#72f88d";
+    // printFilterResult(result);
   } catch (error) {
     console.log(error);
   }
