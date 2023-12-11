948850d1c88656c1bdc85294dfdcf3c309a1163d
Elise Marie Hogsnes <elise-marie.h@hotmail.com>
2023-12-11 21:47:36 +0100

    change countdown section for specific listings

--- src/js/dateCountdown.js
+++ src/js/dateCountdown.js
@@ -20,7 +20,7 @@ function getCountdownDate(listing, infoContainer, endData) {
   }
 }
 
-function getPreciseCountdownDate(listing, infoContainer, endData) {
+function getPreciseCountdownDate(listing, daysSection, hoursSection, minSection, secSection, endData) {
   try {
     setInterval(function () {
       const listingdate = new Date(listing.endsAt).getTime();
@@ -32,8 +32,33 @@ function getPreciseCountdownDate(listing, infoContainer, endData) {
       const minutes = Math.floor((timeDifferent % (1000 * 60 * 60)) / (1000 * 60));
       const seconds = Math.floor((timeDifferent % (1000 * 60)) / 1000);
 
-      const endDateCountdown = days + " Days " + hours + " Hours " + minutes + " Min " + seconds + " Sec";
-      infoContainer.querySelector(endData).innerText = `Ends in: ${endDateCountdown}`;
+      // const endDateCountdown = days + " Days " + hours + " Hours " + minutes + " Min " + seconds + " Sec";
+
+      // const timeSection = document.createElement("div");
+      // const daysSection = document.createElement("div");
+      // const daysText = document.createElement("h2");
+      // const hoursSection = document.createElement("div");
+      // const hoursText = document.createElement("h2");
+      // const minSection = document.createElement("div");
+      // const minText = document.createElement("h2");
+      // const secSection = document.createElement("div");
+      // const secText = document.createElement("h2");
+
+      // infoContainer.append(timeSection);
+      // timeSection.append(daysSection);
+      // daysSection.append(daysText);
+      // timeSection.append(hoursSection);
+      // hoursSection.append(hoursText);
+      // timeSection.append(minSection);
+      // minSection.append(minText);
+      // timeSection.append(secSection);
+      // secSection.append(secText);
+
+      daysSection.querySelector(endData).innerText = `${days}`;
+      hoursSection.querySelector(endData).innerText = `${hours}`;
+      minSection.querySelector(endData).innerText = `${minutes}`;
+      secSection.querySelector(endData).innerText = `${seconds}`;
+      // infoContainer.querySelector(endData).innerText = `Ends in: ${endDateCountdown}`;
     }, 1000);
   } catch (error) {
     console.log(error);
