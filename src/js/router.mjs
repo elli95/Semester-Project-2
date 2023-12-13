355601f5217fc47b6d035e0dccde6f648b777b03
Elise Marie Hogsnes <elise-marie.h@hotmail.com>
2023-12-13 18:31:17 +0100

    Better router

--- src/js/router.mjs
+++ src/js/router.mjs
@@ -5,8 +5,8 @@ import { signupFormListener } from "./handlers/user-register.mjs";
 import { userLoginFormListener } from "./handlers/user-login.mjs";
 
 const path = location.pathname;
-if (path === "/profile/login") {
+if (path === "/profile/login.html" || location.pathname === "/profile/login") {
   userLoginFormListener();
-} else if (path === "/profile/register") {
+} else if (path === "/profile/register.html" || location.pathname === "/profile/register") {
   signupFormListener();
 }
